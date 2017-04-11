05563ca6fa0eeb6434f32d5fafb3e4bd49f4f6f3
James Jenkins <james.p.jenkins@wmich.edu>
2017-04-10 21:38:11 -0400

    Update utils

--- Autopilot_Interface/utils.cpp
+++ Autopilot_Interface/utils.cpp
@@ -5,6 +5,7 @@
 * @author James Jenkins, <james.p.jenkins@wmich.edu>
 */
 #include <stdio.h>
+#include <stdint.h>
 #include <Windows.h>
 
 #include "utils.h"
@@ -15,10 +16,25 @@ void usleep(__int64 usec)
 	HANDLE timer;
 	LARGE_INTEGER ft;
 
-	ft.QuadPart = -(10 * usec); // Convert to 100 nanosecond interval, negative value indicates relative time
+	// Convert to 100 nanosecond interval, negative value indicates relative time
+	ft.QuadPart = -(10 * usec);
 
 	timer = CreateWaitableTimer(NULL, TRUE, NULL);
 	SetWaitableTimer(timer, &ft, 0, NULL, NULL, 0);
 	WaitForSingleObject(timer, INFINITE);
 	CloseHandle(timer);
+}
+
+
+uint64_t get_time_usec()
+{
+	SYSTEMTIME st;
+	FILETIME ft;
+	//static struct timeval _time_stamp;
+	GetSystemTime(&st);
+	SystemTimeToFileTime(&st, &ft);
+	_ULARGE_INTEGER li;
+	li.HighPart = ft.dwHighDateTime;
+	li.LowPart = ft.dwLowDateTime;
+	return li.QuadPart;
 }
--- Autopilot_Interface/utils.h
+++ Autopilot_Interface/utils.h
@@ -2,6 +2,9 @@
 #ifndef _UTILS_H_
 #define _UTILS_H_
 
+#include <stdint.h>
+
 void usleep(__int64 usec);
+uint64_t get_time_usec();
 
 #endif
